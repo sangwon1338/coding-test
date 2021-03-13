6e659fad84d177e220d87b57b81f0e3d56f24873
sangwon1338 <70992303+sangwon1338@users.noreply.github.com>
2021-03-13 22:27:10 +0900

    Update solution.py

--- 프로그래머스 1단계/정수 제곱근 판별/solution.py
+++ 프로그래머스 1단계/정수 제곱근 판별/solution.py
@@ -2,7 +2,6 @@ import math
 def solution(n):
     answer = 0
     n_root = math.sqrt(n)
-    print(type(n_root))
     if n_root == float(int(n_root)):
         answer = math.pow(n_root+1,2)
     else:
